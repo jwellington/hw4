35f5bbe89a721c2c5e0fa193e191fe46c7c7eb5f
jwellington <jwellington@ptc.com>
2011-04-04 16:24:49 -0500

    Initial Commit

---
+++ hw4/dime.h
@@ -0,0 +1,22 @@
+/**************************
+ * dime.h -- the header file for dime.c, a 
+ * distributed make
+ *
+ *
+ *
+ *************************/
+
+#ifndef _DIME_H_
+#define _DIME_H_
+
+#define true 1
+#define false 0
+typedef int bool;
+
+void dime_usage(char*);
+rule_node_t* parse_file(char*);
+void fake_exec(rule_t*);
+void exec_target_rec(rule_t*, rule_node_t*);
+void execute_targets(int, char**, rule_node_t*);
+
+#endif
